c499de437ca9e1a686b58005c6042813c9f1b102
NguyenBaDat99 <55686492+NguyenBaDat99@users.noreply.github.com>
2020-05-26 12:52:56 +0700

    Cải thiện giao diện trang chủ

--- LEnglish.Web/ClientApp/src/app/home/home.component.ts
+++ LEnglish.Web/ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import 'bootstrap';
+declare var $:any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  
 })
 export class HomeComponent {
+
+  ngOnInit() {
+    $('.carousel').carousel({
+        interval: 2000
+    });
+
+    
+
+  }
+  
 }
