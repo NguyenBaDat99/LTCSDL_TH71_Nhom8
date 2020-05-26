384e7785eb2b28eff8b3c94e03092a5e22e11d81
NguyenBaDat99 <55686492+NguyenBaDat99@users.noreply.github.com>
2020-05-26 15:35:47 +0700

    Đang làm reading

--- LEnglish.Web/ClientApp/src/app/reading/reading.component.ts
+++ LEnglish.Web/ClientApp/src/app/reading/reading.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-reading',
   templateUrl: './reading.component.html',
   styleUrls: ['./reading.component.css']
 })
-export class ReadingComponent implements OnInit {
+export class ReadingComponent implements OnInit{
+  public modalID: string;
+  public res: any;
+  public listBaiTap: [];
+  public noiDung: string;
+  public tenBaiTap: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.post('https://localhost:44318/' + 'api/BaiTap/get-all',null).subscribe(result => {
+      this.res = result;
+      this.listBaiTap = this.res.data;
+      console.log(this.listBaiTap);
+    }, error => console.error(error));
+    
   }
 
+  ngOnInit(){
+
+  }
+
+  public createModel(maBT:string){
+    console.log(this.listBaiTap);
+      // this.listBaiTap.forEach(bt => {
+      //   if(bt.maBaiTap = maBT){
+      //     this.noiDung = bt.noiDung;
+      //     this.tenBaiTap = bt.tenBaiTap;
+      //   }
+      // });
+    }
+
 }
+
